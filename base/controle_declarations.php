f3ac1c408f6b69f7400db5552974cb4ce61d3bef
Eric Lupinacci <eric@smellup.net>
2019-08-18 17:52:10 +0200

    Modification de la déclaration des tables.

--- base/controle_declarations.php
+++ base/controle_declarations.php
@@ -7,115 +7,124 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 /**
  * Déclaration des objets du plugin.
  * Le plugin ajoute :
+ * - l'objet contrôle qui correspond à une fonction lancée périodiquement ou à la demande.
  * - l'objet anomalie, produit des contrôles.
  *
  * @pipeline declarer_tables_objets_sql
  *
- * @param array $tables
- *        Description des tables de la base.
+ * @param array $tables Description des tables de la base.
  *
- * @return array
- *        Description des tables de la base complétée par celles du plugin.
+ * @return array Description des tables de la base complétée par celles du plugin.
  */
 function controle_declarer_tables_objets_sql($tables) {
 
-	$tables['spip_anomalies'] = array(
-		'type' => 'anomalie',
+	// Table spip_controles, description des contrôles manuels ou périodiques
+	$tables['spip_controles'] = array(
+		'type'       => 'controle',
 		'principale' => 'oui',
-		'field'=> array(
-			'id_anomalie'   => "bigint(21) NOT NULL",
-			'type_controle' => "varchar(255) NOT NULL default ''",
+		'field'      => array(
+			'id_controle'   => 'bigint(21) NOT NULL',
+			'fonction'      => 'varchar(255) NOT NULL',
+			'type_controle' => 'varchar(255) NOT NULL',
+			'nom'           => "text DEFAULT '' NOT NULL",
+			'descriptif'    => "text DEFAULT '' NOT NULL",
+			'periode'       => 'smallint DEFAULT 0 NOT NULL',
+			'priorite'      => 'smallint(6) NOT NULL default 0',
+			'actif'         => "varchar(3) DEFAULT 'oui' NOT NULL",
+			'date'          => "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			'signature'     => "varchar(32) DEFAULT '' NOT NULL",
+			'maj'           => 'timestamp DEFAULT current_timestamp ON UPDATE current_timestamp',
+		),
+		'key' => array(
+			'PRIMARY KEY'       => 'id_controle',
+			'KEY type_controle' => 'type_controle',
+		),
+		'titre' => 'nom',
+
+		'champs_editables'        => array(),
+		'champs_versionnes'       => array(),
+		'rechercher_champs'       => array(),
+		'tables_jointures'        => array(),
+
+		// Textes standard
+		'texte_retour' 			      => '',
+		'texte_modifier' 		     => '',
+		'texte_creer' 			       => '',
+		'texte_creer_associer' 	=> '',
+		'texte_signale_edition' => '',
+		'texte_objet' 			       => 'controle:titre_controle',
+		'texte_objets' 			      => 'controle:titre_controles',
+		'info_aucun_objet'		    => 'controle:info_aucun_controle',
+		'info_1_objet' 			      => 'controle:info_1_controle',
+		'info_nb_objets' 		     => 'controle:info_nb_controle',
+		'texte_logo_objet' 		   => '',
+	);
+
+	// Table spip_anomalies, les résultats des contrôles
+	$tables['spip_anomalies'] = array(
+		'type'                    => 'anomalie',
+		'principale'              => 'oui',
+		// Déclaration des champs
+		'field'                   => array(
+			'id_anomalie'   => 'bigint(21) NOT NULL',
+			'id_controle'   => 'bigint(21) NOT NULL',
 			'objet'         => "varchar(25) NOT NULL default ''",
-			'id_objet'      => "bigint(21) NOT NULL default 0",
+			'id_objet'      => 'bigint(21) NOT NULL default 0',
 			'gravite'       => "varchar(1) DEFAULT 'e' NOT NULL",
 			'type_anomalie' => "varchar(127) DEFAULT '' NOT NULL",
 			'statut'        => "varchar(10) DEFAULT 'publie' NOT NULL",
 			'parametres'    => "text DEFAULT '' NOT NULL",
 			'date'          => "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
-			'maj'           => "TIMESTAMP",
+			'maj'           => 'timestamp DEFAULT current_timestamp ON UPDATE current_timestamp',
 		),
 		'key' => array(
-			'PRIMARY KEY'       => 'id_anomalie',
-			'KEY type_controle' => 'type_controle',
-			'KEY objet'         => 'objet',
-			'KEY id_objet'      => 'id_objet',
-			'KEY type_anomalie' => 'type_anomalie',
+			'PRIMARY KEY'         => 'id_anomalie',
+			'KEY id_controle'     => 'id_controle',
+			'KEY objet'           => 'objet',
+			'KEY id_objet'        => 'id_objet',
+			'KEY type_anomalie'   => 'type_anomalie',
 		),
 		'join'  => array(
-			'id_anomalie'   => 'id_anomalie',
-			'type_controle' => 'type_controle',
+			'id_anomalie'         => 'id_anomalie',
+			'id_controle'         => 'id_controle',
 		),
-        'titre' => 'gravite-type_erreur : id_anomalie',
-
-        'champs_editables'  => array(),
-        'champs_versionnes' => array(),
-        'rechercher_champs' => array(),
-        'tables_jointures'  => array(),
-        'statut_textes_instituer' => array(
-            'publie'   => 'anomalie:texte_statut_publie',
-            'corrige'  => 'anomalie:texte_statut_corrige',
-            'poubelle' => 'anomalie:texte_statut_poubelle',
-        ),
-        'statut'=> array(
-            array(
-                'champ'     => 'statut',
-                'publie'    => 'publie',
-                'previsu'   => 'publie',
-                'exception' => array('statut', 'tout')
-            )
-        ),
-        'texte_changer_statut' => 'anomalie:texte_changer_statut_anomalie',
-
+		'titre' => 'gravite-type_anomalie : id_anomalie',
+		// Champs spéciaux et jointures
+		'champs_editables'        => array(),
+		'champs_versionnes'       => array(),
+		'rechercher_champs'       => array(),
+		'tables_jointures'        => array(),
+		// Statuts
+		'statut_textes_instituer' => array(
+			'publie'   => 'anomalie:texte_statut_publie',
+			'corrige'  => 'anomalie:texte_statut_corrige',
+			'poubelle' => 'anomalie:texte_statut_poubelle',
+		),
+		'statut'                  => array(
+			array(
+				'champ'     => 'statut',
+				'publie'    => 'publie',
+				'previsu'   => 'publie',
+				'exception' => array('statut', 'tout')
+			)
+		),
+		'texte_changer_statut'    => 'anomalie:texte_changer_statut_anomalie',
 		// Textes standard
-		'texte_retour' 			=> '',
-		'texte_modifier' 		=> '',
-		'texte_creer' 			=> '',
-		'texte_creer_associer' 	=> '',
-		'texte_signale_edition' => '',
-		'texte_objet' 			=> 'anomalie:titre_anomalie',
-		'texte_objets' 			=> 'anomalie:titre_anomalies',
-		'info_aucun_objet'		=> 'anomalie:info_aucun_anomalie',
-		'info_1_objet' 			=> 'anomalie:info_1_anomalie',
-		'info_nb_objets' 		=> 'anomalie:info_nb_anomalie',
-		'texte_logo_objet' 		=> '',
+		'texte_retour' 			  => '',
+		'texte_modifier' 		  => '',
+		'texte_creer' 			  => '',
+		'texte_creer_associer' 	  => '',
+		'texte_signale_edition'   => '',
+		'texte_objet' 			  => 'anomalie:titre_anomalie',
+		'texte_objets' 			  => 'anomalie:titre_anomalies',
+		'info_aucun_objet'		  => 'anomalie:info_aucun_anomalie',
+		'info_1_objet' 			  => 'anomalie:info_1_anomalie',
+		'info_nb_objets' 		  => 'anomalie:info_nb_anomalie',
+		'texte_logo_objet' 		  => '',
 	);
 
 	return $tables;
 }
-
-/**
- * Insertion dans le pipeline declarer_tables_auxiliaires.
- * Déclarer la table auxiliaire spip_controles.
- * 
- * @pipeline declarer_tables_auxiliaires
- * @param array $tables_auxiliaires
- * 		Déclarations des tables pour le compilateur
- * @return array
- * 		Déclarations des tables pour le compilateur
- */
-function controle_declarer_tables_auxiliaires($tables_auxiliaires){
-	
-	$controles = array(
-		'type_controle' => "varchar(255) NOT NULL",
-		'nom'           => "text DEFAULT '' NOT NULL",
-		'descriptif'    => "text DEFAULT '' NOT NULL",
-		'periode'       => "smallint DEFAULT 0 NOT NULL",
-		'actif'         => "varchar(3) DEFAULT 'oui' NOT NULL",
-		'date'          => "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
-		'signature'     => "varchar(32) DEFAULT '' NOT NULL",
-		'maj'           => "TIMESTAMP",
-	);
-	$controles_key = array(
-		'PRIMARY KEY'   => 'type_controle',
-	);
-	$tables_auxiliaires['spip_controles'] = array(
-		'field' => &$controles,
-		'key' => &$controles_key
-	);
-		
-	return $tables_auxiliaires;
-}
-
 
 /**
  * Déclaration des informations tierces (alias, traitements, jointures, etc)
@@ -126,9 +135,10 @@ function controle_declarer_tables_auxiliaires($tables_auxiliaires){
  * @pipeline declarer_tables_interfaces
  *
  * @param array $interface
- * 		Tableau global des informations tierces sur les tables de la base de données
+ *                         Tableau global des informations tierces sur les tables de la base de données
+ *
  * @return array
- *		Tableau fourni en entrée et mis à jour avec les nouvelles informations
+ *               Tableau fourni en entrée et mis à jour avec les nouvelles informations
  */
 function contrib_declarer_tables_interfaces($interface) {
 
--- controle_administrations.php
+++ controle_administrations.php
@@ -3,10 +3,9 @@
  * Ce fichier contient les fonctions de création, de mise à jour et de suppression
  * du schéma de données propres au plugin (tables et configuration).
  */
-
-if (!defined("_ECRIRE_INC_VERSION")) return;
-
-
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 /**
  * Installation du schéma de données propre au plugin et gestion des migrations suivant
  * les évolutions du schéma.
@@ -14,18 +13,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * Le schéma comprend des tables et des variables de configuration.
  *
  * @api
+ *
  * @see controle_declarer_tables_principales()
  * @see controle_declarer_tables_interfaces()
  *
- * @param string $nom_meta_base_version
- * 		Nom de la meta dans laquelle sera rangée la version du schéma
- * @param string $version_cible
- * 		Version du schéma de données en fin d'upgrade
+ * @param string $nom_meta_base_version Nom de la meta dans laquelle sera rangée la version du schéma
+ * @param string $version_cible         Version du schéma de données en fin d'upgrade
  *
  * @return void
  */
-function controle_upgrade($nom_meta_base_version, $version_cible){
-
+function controle_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
 	// Création des tables
@@ -37,23 +34,23 @@ function controle_upgrade($nom_meta_base_version, $version_cible){
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
 }
 
-
 /**
  * Suppression de l'ensemble du schéma de données propre au plugin, c'est-à-dire
  * les tables et les variables de configuration.
  *
  * @api
  *
- * @param string $nom_meta_base_version
- * 		Nom de la meta dans laquelle sera rangée la version du schéma
+ * @param string $nom_meta_base_version Nom de la meta dans laquelle sera rangée la version du schéma
  *
  * @return void
  */
 function controle_vider_tables($nom_meta_base_version) {
 
-	// on efface les tables
-	sql_drop_table("spip_controles");
-	sql_drop_table("spip_anomalies");
+	// On efface les jobs associés aux contrôles (spip_jobs et spip_jobs_liens)
+
+	// On efface les tables des contrôles et anomalies
+	sql_drop_table('spip_controles');
+	sql_drop_table('spip_anomalies');
 
 	// on efface la meta du schéma du plugin
 	effacer_meta($nom_meta_base_version);
