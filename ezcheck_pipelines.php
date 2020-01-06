9e53c3ff39eb123d6863dc305147f78ef8512dda
Eric Lupinacci <eric@smellup.net>
2020-01-06 21:08:30 +0100

    Fonction de chargement des dashboards dans un cache sécurisé

--- ezcheck_pipelines.php
+++ ezcheck_pipelines.php
@@ -8,7 +8,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 /**
  * Ajouter du contenu au centre de la page sur les pages privées.
  *
- * Page d'adminisration des plugin : on recharge les types de contrôle.
+ * Page d'adminisration des plugin :
+ * - on recharge les types de contrôle.
+ * - on recharge les dashboards
  *
  * @param $flux
  *
@@ -22,10 +24,16 @@ function ezcheck_affiche_milieu($flux) {
 
 		if ($exec == 'admin_plugin') {
 			// Administration des plugins
+
 			// On recharge les types de contrôles dont la liste a pu changer. Inutile de forcer un
 			// rechargement complet.
 			include_spip('inc/ezcheck_type_controle');
 			type_controle_charger();
+
+			// On recharge les dashboards dont la liste et le contenu ont pu changer. Inutile de forcer un
+			// rechargement complet.
+			include_spip('inc/ezcheck_dashboard');
+			dashboard_charger();
 		}
 	}
 
--- inc/ezcheck_dashboard.php
+++ inc/ezcheck_dashboard.php
@@ -14,50 +14,43 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  *@api
  *
  */
-function dashboard_charger() {
+function dashboard_charger($recharger = false) {
 
 	// Retour de la fonction
 	$retour = true;
 
-	// On recherche les contrôles directement par leur fichier YAML de configuration car il est
+	// On recherche les dashboards directement par leur fichier YAML de configuration car il est
 	// obligatoire. La recherche s'effectue dans le path en utilisant le dossier relatif fourni.
-	if ($fichiers = find_all_in_path('ezcheck/dasboards', '.+[.]yaml$')) {
-		// Initialisation des tableaux de types de contrôle.
-		$types_controle_a_ajouter = $types_controle_a_changer = $types_controle_a_effacer = array();
+	if ($fichiers = find_all_in_path('ezcheck/dashboards/', '.+[.]yaml$')) {
+		// Initialisation du tableau des dashboard
+		$dashboards = array();
 
-		// Récupération de la description complète des contrôles déjà enregistrés de façon :
-		// - à gérer l'activité des types en fin de chargement
-		// - de comparer les signatures md5 des noisettes déjà enregistrées. Si on force le rechargement il est inutile
+		// Récupération de la description complète des dashboards déjà enregistrés de façon :
+		// - à comparer les signatures md5 des noisettes déjà enregistrées. Si on force le rechargement il est inutile
 		//   de gérer les signatures et les contrôles modifiés ou obsolètes.
-		$types_controle_existants = type_controle_lister();
-		$signatures = array();
-		if (!$recharger) {
-			$signatures = array_column($types_controle_existants, 'signature', 'type_controle');
-			// On initialise la liste des contrôles à supprimer avec l'ensemble des contrôles déjà stockés.
-			$types_controle_a_effacer = $signatures ? array_keys($signatures) : array();
-		}
+		$dashboards_existants = !$recharger ? dashboard_lister() : array();
 
 		foreach ($fichiers as $_squelette => $_chemin) {
-			$type_controle = basename($_squelette, '.yaml');
-			// Si on a forcé le rechargement ou si aucun md5 n'est encore stocké pour le contrôle
-			// on positionne la valeur du md5 stocké à chaine vide.
-			// De cette façon, on force la lecture du fichier JSON/YAML du contrôle.
-			$md5_stocke = (isset($signatures[$type_controle]) and !$recharger)
-				? $signatures[$type_controle]
-				: '';
+			// L'identifiant du dashboard est son nom de fichier sans extension
+			$dashboard_id = basename($_squelette, '.yaml');
 
 			// Initialisation de la description par défaut du type de contrôle
 			$description_defaut = array(
-				'type_controle' => $type_controle,
-				'fonction'      => 'php',
-				'nom'           => $type_controle,
+				'identifiant'   => $dashboard_id,
+				'nom'           => $dashboard_id,
 				'description'   => '',
-				'icone'         => 'controle-24.png',
-				'priorite'      => 0,
-				'periode'       => 0,
-				'actif'         => 'oui',
+				'icone'         => 'dashboard-24.png',
+				'boite'         => '',
+				'groupes'       => array(),
 				'signature'     => '',
 			);
+
+			// Si on a forcé le rechargement ou si aucun md5 n'est encore stocké pour le dashboard
+			// on positionne la valeur du md5 stocké à chaine vide.
+			// De cette façon, on force la lecture du fichier JSON/YAML du dashboard.
+			$md5_stocke = (isset($dashboards_existants[$dashboard_id]['signature']) and !$recharger)
+				? $dashboards_existants[$dashboard_id]['signature']
+				: '';
 
 			// On vérifie que le md5 du fichier JSON/YAML est bien différent de celui stocké avant de charger
 			// le contenu. Sinon, on passe au fichier suivant.
@@ -71,56 +64,29 @@ function dashboard_charger() {
 				// Complétude de la description avec les valeurs par défaut
 				$description = array_merge($description_defaut, $description);
 
-				if (!$md5_stocke or $recharger) {
-					// Le type de noisette est soit nouveau soit on est en mode rechargement forcé:
-					// => il faut le rajouter.
-					$types_controle_a_ajouter[] = $description;
-				} else {
-					// La description stockée a été modifiée et le mode ne force pas le rechargement:
-					// => il faut mettre à jour le type de noisette.
-					$types_controle_a_changer[] = $description;
-					// => et il faut donc le supprimer de la liste de types de noisette obsolètes
-					$types_controle_a_effacer = array_diff($types_controle_a_effacer, array($type_controle));
-				}
+				// On ajoute le dashboard nouveau ou modifié
+				$dashboards[$dashboard_id] = $description;
 			} else {
-				// Le type de noisette n'a pas changé et n'a donc pas été rechargé:
-				// => Il faut donc juste indiquer qu'il n'est pas obsolète.
-				$types_controle_a_effacer = array_diff($types_controle_a_effacer, array($type_controle));
+				// Le dashboard n'est pas modifié
+				// => Il faut l'ajouter tel quel dans le tableau
+				$dashboards[$dashboard_id] = $dashboards_existants[$dashboard_id];
 			}
 		}
 
-		// Mise à jour des contrôles en base de données :
-		// -- Suppression des contrôles obsolètes ou de tous les contrôles si on est en mode rechargement forcé.
-		// -- Update des contrôles modifiés.
-		// -- Insertion des nouveaux contrôles.
+		// Etant donné que le nombre dashboard est réduit tout comme les informations qui le compose on choisit
+		// de l'écrire systématiquement.
+		// -- Initialisation de l'identifiant du cache des dashboards
+		$cache = array(
+			'nom' => 'dashboards',
+		);
 
-		// Mise à jour de la table des contrôles
-		$from = 'spip_types_controles';
-		// -- Suppression des pages obsolètes ou de toute les pages non virtuelles si on est en mode
-		//    rechargement forcé.
-		if (sql_preferer_transaction()) {
-			sql_demarrer_transaction();
-		}
-		if ($types_controle_a_effacer) {
-			sql_delete($from, sql_in('type_controle', $types_controle_a_effacer));
-		} elseif ($recharger) {
-			sql_delete($from);
-		}
-		// -- Update des contrôels modifiés
-		if ($types_controle_a_changer) {
-			sql_replace_multi($from, $types_controle_a_changer);
-		}
-		// -- Insertion des nouveaux contrôles
-		if ($types_controle_a_ajouter) {
-			sql_insertq_multi($from, $types_controle_a_ajouter);
-		}
-		if (sql_preferer_transaction()) {
-			sql_terminer_transaction();
-		}
+		// Ecriture du cache sécurisé
+		cache_ecrire('ezcheck', $cache, $dashboards);
 	}
 
 	return $retour;
 }
+
 
 /**
  * Renvoie l'information brute demandée pour l'ensemble des contrôles utilisés
